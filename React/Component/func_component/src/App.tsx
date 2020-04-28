56f19d713e18d5f36e812d584b2b670869c3c441
kf3225 <clrflg.k.f@ezweb.ne.jp>
2020-04-28 21:43:17 +0900

    functional component

---
+++ React/Component/func_component/src/App.tsx
@@ -0,0 +1,37 @@
+import React, { FC } from "react";
+
+import CharacterList, { Character } from "./CharacterList";
+import "./App.css";
+
+const App: FC<{}> = () => {
+  const characters: Character[] = [
+    {
+      id: 1,
+      name: "AAA",
+      age: 10,
+      height: 150,
+    },
+    {
+      id: 2,
+      name: "BBB",
+      age: 20,
+      height: 160,
+    },
+    {
+      id: 3,
+      name: "CCC",
+      age: 30,
+    },
+  ];
+
+  return (
+    <div className="container">
+      <header>
+        <h1>Characters</h1>
+      </header>
+      <CharacterList school="ABC" characterList={characters} />
+    </div>
+  );
+};
+
+export default App;

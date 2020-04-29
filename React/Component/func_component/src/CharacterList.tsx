2a276c514725d11c114c6b340be54fbe81acebaa
KF <34534526+kf3225@users.noreply.github.com>
2020-04-29 18:46:05 +0900

    Func component (#19)
    * functional component
    
    * unknown

---
+++ React/Component/func_component/src/CharacterList.tsx
@@ -0,0 +1,37 @@
+import React, { FC } from "react";
+import { Header, Icon, Item } from "semantic-ui-react";
+
+export interface Character {
+  id: number;
+  name: string;
+  age: number;
+  height?: number;
+}
+
+interface CharacterListProps {
+  school?: string;
+  characterList: Character[];
+}
+
+const CharacterList: FC<CharacterListProps> = ({
+  school = "unknown",
+  characterList,
+}) => (
+  <>
+    <Header as="h2">{school}</Header>
+    <Item.Group>
+      {characterList.map((c) => (
+        <Item key={c.id}>
+          <Icon name="user circle" size="huge" />
+          <Item.Content>
+            <Item.Header>name : {c.name}</Item.Header>
+            <Item.Meta>age : {c.age}</Item.Meta>
+            <Item.Meta>height : {c.height ? c.height : "???"} cm</Item.Meta>
+          </Item.Content>
+        </Item>
+      ))}
+    </Item.Group>
+  </>
+);
+
+export default CharacterList;
